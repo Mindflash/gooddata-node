ba27cc00c6a0814ee5bab3cce35c0a54be2eb07c
sathiz <sathizz@gmail.com>
2018-06-18 09:58:34 -0700

    chore: remove spread operator code to support node6

--- src/util/route-helper.js
+++ src/util/route-helper.js
@@ -7,28 +7,28 @@ class RouteHelper {
     return template.split('/').map(view => (view[0] === ':' ? params[view.substr(1)] : view)).join('/') + queryStr;
   }
 
-  parse(route, template) {
-    let parsedRoute = '';
-    if (route.startsWith('http')) {
-      const routeMatch = route.match(/^(https?:)\/\/(([^:/?#]*)(?::([0-9]+))?)([/]{0,1}[^?#]*)(\?[^#]*|)(#.*|)$/);
-      parsedRoute = routeMatch ? routeMatch[5] : '';
-    } else {
-      parsedRoute = route;
-    }
-
-    const values = parsedRoute.split('/');
-    const views = template.split('/');
-
-    return views.reduce((result, view, idx) => {
-      if (view[0] === ':') {
-        return {
-          ...result,
-          [view.substr(1)]: values[idx]
-        };
-      }
-      return result;
-    }, {});
-  };
+  // parse(route, template) {
+  //   let parsedRoute = '';
+  //   if (route.startsWith('http')) {
+  //     const routeMatch = route.match(/^(https?:)\/\/(([^:/?#]*)(?::([0-9]+))?)([/]{0,1}[^?#]*)(\?[^#]*|)(#.*|)$/);
+  //     parsedRoute = routeMatch ? routeMatch[5] : '';
+  //   } else {
+  //     parsedRoute = route;
+  //   }
+  //
+  //   const values = parsedRoute.split('/');
+  //   const views = template.split('/');
+  //
+  //   return views.reduce((result, view, idx) => {
+  //     if (view[0] === ':') {
+  //       return {
+  //         ...result,
+  //         [view.substr(1)]: values[idx]
+  //       };
+  //     }
+  //     return result;
+  //   }, {});
+  // };
 }
 
 module.exports = RouteHelper;
